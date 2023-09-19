7296184ffa3adb2e762c6976ea6b573b328a3ef4
ChillingCorn <143270073+ChillingCorn@users.noreply.github.com>
2023-09-19 19:18:17 +0700

    Add files via upload

---
+++ c++/test/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,m,k;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("test.inp","r",stdin);
+    freopen("test.out","w",stdout);
+    cin>>n>>m>>k;
+    cout<<n<<" "<<m<<" "<<k;
+    123 
+}
